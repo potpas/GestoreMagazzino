36a1f4ad773a03a9e47cf94df5e03428964ec23a
datto <datto@LAPTOP-PFU8NVMG>
2019-05-20 18:19:23 +0200

    Prodotto finale.

--- GestoreMagazzino/src/logica/Prodotto.java
+++ GestoreMagazzino/src/logica/Prodotto.java
@@ -2,73 +2,125 @@
 package logica;
 
 public class Prodotto {
-       private double prezzo;
-       private String marca;
-       private String codice;
-       private String modello;
-       private int AnnoDiProduzione;
-       private String tipo;
-       
-	public Prodotto(double prezzo, String marca, String codice, String modello, int annoDiProduzione, String tipo) {
-		super();
-		this.prezzo = prezzo;
-		this.marca = marca;
-		this.codice = codice;
-		this.modello = modello;
-		this.AnnoDiProduzione = annoDiProduzione;
-		this.tipo = tipo;
+    private String codice;
+    private String tipo;	
+    private String marca;
+    private String modello;
+    private int annoDiProduzione;
+    private double prezzo;
+    
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Prodotto other = (Prodotto) obj;
+		if (annoDiProduzione != other.annoDiProduzione)
+			return false;
+		if (codice == null) {
+			if (other.codice != null)
+				return false;
+		} else if (!codice.equals(other.codice))
+			return false;
+		if (marca == null) {
+			if (other.marca != null)
+				return false;
+		} else if (!marca.equals(other.marca))
+			return false;
+		if (modello == null) {
+			if (other.modello != null)
+				return false;
+		} else if (!modello.equals(other.modello))
+			return false;
+		if (Double.doubleToLongBits(prezzo) != Double.doubleToLongBits(other.prezzo))
+			return false;
+		if (tipo == null) {
+			if (other.tipo != null)
+				return false;
+		} else if (!tipo.equals(other.tipo))
+			return false;
+		return true;
 	}
 
-	public double getPrezzo() {
-		return prezzo;
-	}
 
-	public void setPrezzo(double prezzo) {
-		this.prezzo = prezzo;
-	}
-
-	public String getMarca() {
-		return marca;
-	}
-
-	public void setMarca(String marca) {
-		this.marca = marca;
-	}
 
 	public String getCodice() {
 		return codice;
 	}
 
+
+
 	public void setCodice(String codice) {
 		this.codice = codice;
 	}
 
-	public String getModello() {
-		return modello;
-	}
 
-	public void setModello(String modello) {
-		this.modello = modello;
-	}
-
-	public int getAnnoDiProduzione() {
-		return AnnoDiProduzione;
-	}
-
-	public void setAnnoDiProduzione(int annoDiProduzione) {
-		AnnoDiProduzione = annoDiProduzione;
-	}
 
 	public String getTipo() {
 		return tipo;
 	}
 
+
+
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
-	
-	
+
+
+
+	public String getMarca() {
+		return marca;
+	}
+
+
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+
+
+	public String getModello() {
+		return modello;
+	}
+
+
+
+	public void setModello(String modello) {
+		this.modello = modello;
+	}
+
+
+
+	public int getAnnoDiProduzione() {
+		return annoDiProduzione;
+	}
+
+
+
+	public void setAnnoDiProduzione(int annoDiProduzione) {
+		this.annoDiProduzione = annoDiProduzione;
+	}
+
+
+
+	public double getPrezzo() {
+		return prezzo;
+	}
+
+
+
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+
+
+
 	public void stampa() {
-		System.out.println( "Prodotto: " + codice + "," + tipo + "," + marca + "," + modello + "," + AnnoDiProduzione + "," + prezzo );
+		System.out.println( "Prodotto: " + codice + "," + tipo + "," + marca + "," + modello + "," + annoDiProduzione + "," + prezzo );
 	}       
 }
